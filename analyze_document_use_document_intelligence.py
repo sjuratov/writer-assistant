377c40e7ad12ccef0057e177b486ff24fc199008
Sasa Juratovic <sasa.juratovic@microsoft.com>
2024-07-24 17:23:01 +0200

    PDF files need to be local. Referencing PDF's via URL is problematic for various reasons

--- analyze_document_use_document_intelligence.py
+++ analyze_document_use_document_intelligence.py
@@ -31,20 +31,27 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-def analyze_documents_output_in_markdown(url: str = "https://cob.silverchair-cdn.com/cob/content_public/journal/jcs/131/14/10.1242_jcs.186254/8/jcs186254.pdf?Expires=1724822537&Signature=SEdkl~B~m8Z8AeMboguMRS4WgAeHACgmN4CEb5bsbtfjN1mIIg2sjLVwkKaB0o5Ys1YGTut8jZjjHqPY6eZtnWr8CDqBrRLGb-N6QQzp7i9qBQBE1fOk86749~2o8gNWkITatoCfDGheDwMcbJo-IrCy4xB1NjbfMdhg1xhRIJDS8aErZsuS2p4nm6m28ycSk-qIB9BxIBp~NIMMRlWb90WmaI~oX2fuspGxmvsFlLni5m5fXJsZxH-ea5mxucnHXiVX2XZOoFwMW5rhUlf6n8oveiCPvHrPusddL6LsLfRHrDNlIj-EdtRcmwEu7PZSp0V~R4UnHXx1ram7zWk1UQ__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA"):
+def analyze_documents_output_in_markdown(document: str):
 
     from azure.core.credentials import AzureKeyCredential
     from azure.ai.documentintelligence import DocumentIntelligenceClient
-    from azure.ai.documentintelligence.models import AnalyzeDocumentRequest, ContentFormat, AnalyzeResult
+    from azure.ai.documentintelligence.models import ContentFormat, AnalyzeResult
+    import base64
 
     endpoint = os.environ["FORM_RECOGNIZER_ENDPOINT"]
     key = os.environ["FORM_RECOGNIZER_KEY"]
-    url = url
+
+    with open(document, "rb") as f:
+        base64_encoded_pdf = base64.b64encode(f.read()).decode("utf-8")
+
+    analyze_request = {
+        "base64Source": base64_encoded_pdf
+    }
 
     document_intelligence_client = DocumentIntelligenceClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     poller = document_intelligence_client.begin_analyze_document(
         "prebuilt-layout",
-        AnalyzeDocumentRequest(url_source=url),
+        analyze_request=analyze_request,
         output_content_format=ContentFormat.MARKDOWN,
     )
     result: AnalyzeResult = poller.result()
@@ -53,8 +60,10 @@ def analyze_documents_output_in_markdown(url: str = "https://cob.silverchair-cdn
 
 if __name__ == "__main__":
 
+    document = "documents/2407.16375v1.pdf"
+
     try:
-        result = analyze_documents_output_in_markdown()
+        result = analyze_documents_output_in_markdown(document)
         print(f"Here's the full content in format {result.content_format}:\n")
         print(result.content)
     except Exception as error:
