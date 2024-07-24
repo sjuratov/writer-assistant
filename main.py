3d3fdeeffbe86e94c668b52fc8c8c2957fd532a9
Sasa Juratovic <sasa.juratovic@microsoft.com>
2024-07-24 10:12:07 +0200

    Added logic to pass URL to PDF document as command line parameter

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ DESCRIPTION:
     This sample demonstrates how to write an abstract based on scientific paper.
 
 USAGE:
-    python main.py
+    python main.py [url]
 
     Set the environment variables with your own values before running the sample:
-    1) url - points to PDF version of scientific paper.
+    1) url - points to PDF version of scientific paper. This is now an optional command line argument.
 """
 
 import time
 import colorama
 from colorama import Fore
+import sys
 
 from analyze_document_use_document_intelligence import analyze_documents_output_in_markdown
 from write_assistant import create_document
@@ -32,8 +33,12 @@ print(Fore.GREEN + "Document analysis is in progress...")
 
 start_time = time.time()
 
-#url = "https://cob.silverchair-cdn.com/cob/content_public/journal/jcs/131/14/10.1242_jcs.186254/8/jcs186254.pdf?Expires=1724822537&Signature=SEdkl~B~m8Z8AeMboguMRS4WgAeHACgmN4CEb5bsbtfjN1mIIg2sjLVwkKaB0o5Ys1YGTut8jZjjHqPY6eZtnWr8CDqBrRLGb-N6QQzp7i9qBQBE1fOk86749~2o8gNWkITatoCfDGheDwMcbJo-IrCy4xB1NjbfMdhg1xhRIJDS8aErZsuS2p4nm6m28ycSk-qIB9BxIBp~NIMMRlWb90WmaI~oX2fuspGxmvsFlLni5m5fXJsZxH-ea5mxucnHXiVX2XZOoFwMW5rhUlf6n8oveiCPvHrPusddL6LsLfRHrDNlIj-EdtRcmwEu7PZSp0V~R4UnHXx1ram7zWk1UQ__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA"
-url = "https://www.nature.com/articles/s41586-024-07701-9.pdf"
+# Check if a URL is provided as a command line argument
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    #url = "https://cob.silverchair-cdn.com/cob/content_public/journal/jcs/131/14/10.1242_jcs.186254/8/jcs186254.pdf?Expires=1724822537&Signature=SEdkl~B~m8Z8AeMboguMRS4WgAeHACgmN4CEb5bsbtfjN1mIIg2sjLVwkKaB0o5Ys1YGTut8jZjjHqPY6eZtnWr8CDqBrRLGb-N6QQzp7i9qBQBE1fOk86749~2o8gNWkITatoCfDGheDwMcbJo-IrCy4xB1NjbfMdhg1xhRIJDS8aErZsuS2p4nm6m28ycSk-qIB9BxIBp~NIMMRlWb90WmaI~oX2fuspGxmvsFlLni5m5fXJsZxH-ea5mxucnHXiVX2XZOoFwMW5rhUlf6n8oveiCPvHrPusddL6LsLfRHrDNlIj-EdtRcmwEu7PZSp0V~R4UnHXx1ram7zWk1UQ__&Key-Pair-Id=APKAIE5G5CRDK6RD3PGA"
+    url = "https://www.nature.com/articles/s41586-024-07701-9.pdf"
 
 try:
     result = analyze_documents_output_in_markdown(url)
