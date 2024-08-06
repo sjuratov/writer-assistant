6bd1019e61be2dcf8842f1a2605c7f7308b942d3
Sasa Juratovic <sasa.juratovic@microsoft.com>
2024-08-06 11:52:29 +0200

    chore: Add streamlit-pdf-viewer dependency and update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,9 +1,8 @@
 import os
 import time
 import streamlit as st
-
+from streamlit_pdf_viewer import pdf_viewer
 from modules.write_assistant import create_document
-
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -35,26 +34,42 @@ def analyze_documents_output_in_markdown(f):
     return result
 
 def main():
+    st.set_page_config(layout="centered")
     st.title("Write Assistant")
 
-    file = st.file_uploader("Upload PDF file that will be used for abstract creation:", type=["pdf"])
+    if 'pdf_ref' not in st.session_state:
+        st.session_state.pdf_ref = None
 
-    start_time = time.time()
+    file = st.file_uploader("Upload PDF file that will be used for abstract creation:", type=["pdf"], key='pdf')
+
+    if st.session_state.pdf:
+        st.session_state.pdf_ref = st.session_state.pdf
 
     if file:
-        st.divider()
-        st.info("Analyzing the document ...", icon="⏳")
-        doc = analyze_documents_output_in_markdown(file)
-        analysis_time = time.time() - start_time
-        st.info(f"Analysis completed in {analysis_time:.2f} seconds.", icon="✅")
 
-        st.info("Creating an abstract of the document ...", icon="⏳")
-        abstract_start_time = time.time()
-        abstract = create_document(doc.content)
-        abstract_time = time.time() - abstract_start_time
-        st.info(f"Abstract created in {abstract_time:.2f} seconds.", icon="✅")
-        st.divider()
-        st.markdown(abstract.choices[0].message.content)
+        tab1, tab2 = st.tabs(["Input Document", "Generated Abstract"])
+
+        with tab1:
+            if st.session_state.pdf_ref:
+                binary_data = st.session_state.pdf_ref.getvalue()
+                pdf_viewer(input=binary_data, width=700, height=800)
+
+        with tab2:
+            start_time = time.time()
+
+            st.info("Analyzing the document ...", icon="⏳")
+            doc = analyze_documents_output_in_markdown(file)
+            analysis_time = time.time() - start_time
+            st.info(f"Analysis completed in {analysis_time:.2f} seconds.", icon="✅")
+
+            st.info("Creating an abstract of the document ...", icon="⏳")
+            abstract_start_time = time.time()
+            abstract = create_document(doc.content)
+            abstract_time = time.time() - abstract_start_time
+            st.info(f"Abstract created in {abstract_time:.2f} seconds.", icon="✅")
+            
+            st.divider()
+            st.markdown(abstract.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
